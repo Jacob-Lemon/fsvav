18dd9a3f903d0a0ded2451d3e0d20b7e5e9426df
easton-mcbeth <eastonmcbeth1@gmail.com>
2025-06-13 10:30:47 -0600

    removed unecessary comments

--- fsvav/fsvav.srcs/sources_1/new/mux.vhd
+++ fsvav/fsvav.srcs/sources_1/new/mux.vhd
@@ -1,44 +1,14 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/13/2025 09:41:19 AM
--- Design Name: 
--- Module Name: mux - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity mux is
     port (
         -- d0, d1, sel    : in bit;
-        d0   : in bit;
-        d1   : in bit;
-        sel  : in bit;
-        mout : out bit
+        d0   : in std_logic;
+        d1   : in std_logic;
+        sel  : in std_logic;
+        mout : out std_logic
     );
 end mux;
 
