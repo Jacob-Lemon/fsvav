fe475b3741cf41419bcabbbca66978c142c01c00
easton-mcbeth <eastonmcbeth1@gmail.com>
2025-07-25 10:58:41 -0600

    finished pixel gen

--- src/pixel_generation.vhd
+++ src/pixel_generation.vhd
@@ -8,46 +8,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity pixel_generation is
     port (
         -- inputs
-        clk : in std_logic;
-        reset : in std_logic;
-        video_on : in std_logic;
-        x, y : in std_logic_vector (9 downto 0);
+        clk        : in  std_logic;
+        reset      : in  std_logic;
+        video_on   : in  std_logic;
+        x, y       : in  std_logic_vector(9 downto 0);
         -- outputs
-        rgb : out std_logic_vector (11 downto 0)
+        rgb        : out std_logic_vector(11 downto 0)
     );
 end pixel_generation;
 
 
 architecture Behavioral of pixel_generation is
 
-    signal video_active : std_logic;
-    signal color_data_rgb : std_logic_vector(11 downto 0);
+    -- constant syntax
+    constant blue : std_logic_vector(11 downto 0) := x"00F";
 
 begin
 
-    color_data_rgb <= x"001";
-
-    process(clk, reset) is
+    pixel_gen_proc: process(clk, reset)
     begin
-        if rising_edge(clk) then
-            if (reset)
-                video_active <= 0;
-            else
-                video_active <= video_on;
-            end if;
-        end if;
-    end process;
+        if (reset = '1') then
+            rgb <= x"000";
 
-    process(clk, reset) is
-    begin
-        if rising_edge(clk) then
-            if (reset)
-                rbg <= x"000";
+        elsif rising_edge(clk) then
+            
+            if (video_on = '1') then
+                -- if video on output color
+                rgb <= blue;
             else
-                rbg <= color_data_rgb;
+                -- ptherwise output black when screen is off
+                rgb <= x"000";
             end if;
-        end if;
-    end process;
 
+        end if;
+    end process pixel_gen_proc;
 
 end Behavioral;
